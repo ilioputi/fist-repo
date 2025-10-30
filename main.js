4007f448126d7e7f14e50b10dd39fb99f9c5ad86
Ilia <ilioput755@gmail.com>
2025-10-30 17:54:38 +0300

    Добавил снятие денег

--- main.js
+++ main.js
@@ -2,3 +2,19 @@ let balance = 1000;
 function showBalance(){
     console.log(`Ваш баланс: ${balance} рублей`)
 }
+function withdrawMoney() {  
+    let amount = parseInt(prompt("Введите сумму для снятия:"));  
+      
+    if (isNaN(amount) || amount <= 0) {  
+        console.log("Некорректная сумма!");  
+        return;  
+    }  
+      
+    if (amount > balance) {  
+        console.log("Недостаточно средств на счете!");  
+    } else {  
+        balance -= amount;  
+        console.log(`Успешно снято ${amount} рублей.`);  
+        showBalance();  
+    }  
+}  
