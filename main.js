9ba178041254d4f7110bd4ba45da7d3f4dcde65a
Ilia <ilioput755@gmail.com>
2025-10-30 18:05:27 +0300

    Меню банкомата

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 let balance = 1000;
+
+
 function showBalance(){
     console.log(`Ваш баланс: ${balance} рублей`)
 }
+
+
 function withdrawMoney() {  
     let amount = parseInt(prompt("Введите сумму для снятия:"));  
       
@@ -18,6 +22,8 @@ function withdrawMoney() {
         showBalance();  
     }  
 }
+
+
 function depositMoney() {  
     let amount = parseInt(prompt("Введите сумму для пополнения:"));  
       
@@ -29,4 +35,31 @@ function depositMoney() {
     balance += amount;  
     console.log(`Успешно внесено ${amount} рублей.`);  
     showBalance();  
-}    
+}
+while (true) {  
+    console.log("\nМеню банкомата:");  
+    console.log("1. Проверить баланс");  
+    console.log("2. Снять деньги");  
+    console.log("3. Пополнить счет");  
+    console.log("4. Выйти");  
+      
+    let choice = prompt("Выберите действие:");  
+      
+    switch(choice) {  
+        case '1':  
+            showBalance();  
+            break;  
+        case '2':  
+            withdrawMoney();  
+            break;  
+        case '3':  
+            depositMoney();  
+            break;  
+        case '4':  
+            console.log("До свидания!");  
+            process.exit(); // Выход из программы  
+            break;  
+        default:  
+            console.log("Неверный выбор. Попробуйте еще раз.");  
+    }  
+}      
